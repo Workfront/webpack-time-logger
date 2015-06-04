2b456ab8d2dda02085560f65600070f607bf5f7d
Jeremy Lund <jeremylund@attask.com>
2015-06-04 12:36:46 -0600

    feat: Convert time-logger-nav.html reference to use require().

--- time-logger/time-logger-nav.directive.js
+++ time-logger/time-logger-nav.directive.js
@@ -3,7 +3,7 @@ require('./time-logger-nav-style.css');
 function TimeLoggerNavDirective() {
   return {
     restrict: 'E',
-    templateUrl: 'time-logger/time-logger-nav.html',
+    template: require('./time-logger-nav.html'),
     scope: {
       date: '='
     }
