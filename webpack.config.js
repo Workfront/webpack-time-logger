9ba4c1ccd57be45a38c978e147ad440f6e50d22e
Jeremy Lund <jeremylund@attask.com>
2015-12-17 16:29:31 -0700

    chore: add linting support.

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
   },
   devtool: 'source-map',
   module: {
+    preLoaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'jshint-loader' }
+    ],
     loaders: [
       {test: /\.js$/, loader: 'ng-annotate'},
       {test: /\.css$/, loaders: ['style', 'css']},
