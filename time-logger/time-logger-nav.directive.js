bdf41ed26fc767c30c3ae24cc61e62b2257e8cc6
Jeremy Lund <jeremylund@attask.com>
2015-12-17 16:31:35 -0700

    feat: convert usage of time-logger-nav.html to use require() call.

--- time-logger/time-logger-nav.directive.js
+++ time-logger/time-logger-nav.directive.js
@@ -3,7 +3,8 @@ require('./time-logger-nav-style.css');
 function TimeLoggerNavDirective() {
   return {
     restrict: 'E',
-    templateUrl: 'time-logger/time-logger-nav.html',
+    //templateUrl: 'time-logger/time-logger-nav.html',
+    template: require('./time-logger-nav.html'), // require the HTML into the bundle.
     scope: {
       date: '='
     }
